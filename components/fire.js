f7c9e8824d4123bf905eee0aa87b11fc33d18295
ito-soichro <queen.fm1235@gmail.com>
2021-09-22 11:06:39 +0900

    add firebase

---
+++ components/fire.js
@@ -0,0 +1,14 @@
+import firebase from "firebase"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAcEkn4GocXQpJtC-Y0CJl5v2FDoM5x94Q",
+    authDomain: "ito-react.firebaseapp.com",
+    projectId: "ito-react",
+    storageBucket: "ito-react.appspot.com",
+    messagingSenderId: "410948514330",
+    appId: "1:410948514330:web:33784d12cf127b2f0d17a3"
+  };
+
+  if(firebase.apps.length == 0){
+      firebase.initializeApp(firebaseConfig)
+  }
